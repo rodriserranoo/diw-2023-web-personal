cf8744b3e4c81283bc1ab1d80f2e1e2719331493
Erik <erikalejjandro1112@gmail.com>
2023-10-19 12:22:13 +0200

    Primera Subida

---
+++ ealejand1/PaginaWebPersonal/js/Boton.js
@@ -0,0 +1,10 @@
+
+const body = document.getElementsByTagName('body');
+
+function cambiarTema() {
+  if(body[0].classList.contains('tema-claro')){
+    body[0].classList.replace('tema-claro','tema-oscuro')
+  }else{
+    body[0].classList.replace('tema-oscuro','tema-claro')
+  }
+};
