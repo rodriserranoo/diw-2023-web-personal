b7a9ea18dd348dbfbbee97c281dde4211d497983
pegaso <mhp74185@gmail.com>
2023-10-19 12:18:44 +0200

    Inicio

---
+++ pegasojuegos/js/javascript.js
@@ -0,0 +1,9 @@
+const body = document.getElementsByTagName('body');
+
+function cambiarTema() {
+  if(body[0].classList.contains('tema-claro')){
+    body[0].classList.replace('tema-claro','tema-oscuro')
+  }else{
+    body[0].classList.replace('tema-oscuro','tema-claro')
+  }
+};
